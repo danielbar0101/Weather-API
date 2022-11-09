cf0237551ad479012281d39c58a555e69d8316ef
Daniel Bar <danielbar@gmail.com>
2022-11-09 15:47:50 +0200

    Added feelsLikeCel

--- main.py
+++ main.py
@@ -21,21 +21,28 @@ def kelvinToCelsius(kelvin):
 def kelvinToFahrenheit(kelvin):
     return kelvin * 1.8 - 459.67
 
-
 # defining object to be pulled from the API
 temp_kelvin = response['main']['temp']
 temp_cel = kelvinToCelsius(temp_kelvin)
 humidity = response['main']['humidity']
 feels_Like = response['main']['feels_like']
-#desc = response['weather']['description']
+feelsLikeCel = kelvinToCelsius(feels_Like)
+# desc = {response['weather']['description']}
 country = response['sys']['country']
 sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
 sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
 
+def recommendation(temp):
+    if temp < 18:
+        print("we recommend getting a coat")
+    else:
+        print("it's nice outside :)")
+
+
 # Printing the desired info from the API
 print(f'Below are the stats for {CITY} in {country}')
-print(f"the temperature in Celsius is: {temp_cel:.2f}°C")
-print(f'But it actually feels like: {feels_Like}°C')
+print(f"The temperature in Celsius is: {temp_cel:.2f}°C")
+print(f'But it actually feels like: {feelsLikeCel:.2f}°C')
 print(f"The humidity is: {humidity}%")
 print(f"The sun sets in {CITY} at {sunset_time}")
 print(f"The sun rises in {CITY} at {sunrise_time}")
